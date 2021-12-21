8d5500b5547b71e23ddb4b662c496ae21a833355
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-12-21 15:03:03 +0100

    feat(server): create status handler
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,16 @@
 mod config;
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, HttpResponse, HttpServer};
 
-#[get("/{id}/{name}/index.html")]
-async fn index(path: web::Path<(u32, String)>) -> impl Responder {
-    let (id, name) = path.into_inner();
-    format!("Hello {}! id:{}", name, id)
+#[get("/api")]
+async fn status() -> HttpResponse {
+    HttpResponse::NoContent().finish()
+}
+
+macro_rules! create_app {
+    () => {
+        actix_web::App::new().service(crate::status)
+    };
 }
 
 #[actix_web::main]
@@ -14,8 +19,33 @@ async fn main() -> std::io::Result<()> {
     config.init_logger();
     let address = config.address();
     tracing::info!("starting server on {}", address);
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| create_app!())
         .bind(config.address())?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_http::Request;
+    use actix_web::dev::ServiceResponse;
+    use actix_web::http::StatusCode;
+    use actix_web::test;
+
+    #[derive(Default)]
+    pub struct TestServer;
+
+    impl TestServer {
+        pub async fn execute(&self, req: Request) -> ServiceResponse {
+            let app = test::init_service(create_app!()).await;
+            test::call_service(&app, req).await
+        }
+    }
+
+    #[actix_rt::test]
+    async fn status() {
+        let req = test::TestRequest::get().uri("/api").to_request();
+        let res = TestServer::default().execute(req).await;
+        assert_eq!(res.status(), StatusCode::NO_CONTENT);
+    }
 }
